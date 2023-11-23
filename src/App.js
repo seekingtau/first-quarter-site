88d44e7f29585aab9552ba19a2d040f26250a0c7
seekingtau <cameron@elda.bio>
2023-11-23 15:55:58 -0500

    commit new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
+import herotexthome from './static/images/hero-text-home.svg';
+import arrow from './static/images/arrow.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container home">
+        <div className="navbar-container">
+            <div className="navbar-item">
+                <p>
+                  <a href="/" className="styled-link">homepage</a>
+                </p>
+                <p>
+                  <a href="/about" className="styled-link">about</a>
+                </p>
+                <p>
+                  <a href="/partners" className="styled-link">partners</a>
+                </p>
+                <p>
+                  <a href="/initiatives" className="styled-link">initiatives</a>
+                </p>
+                <p>
+                  <a href="/contribute" className="styled-link">contribute</a>
+                </p>
+            </div>
+        </div>
+        <div className="hero-container">
+            <img src={herotexthome} width="60%"/>
+            <div className="button-container">
+                <p>our thesis</p>
+                <img src={arrow} width="16px"/>
+            </div>
+        </div>
     </div>
   );
 }
